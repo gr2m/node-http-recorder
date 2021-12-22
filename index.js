2e370c9ba6ed371caffb8547a61935dc7fc8bdc9
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-12-21 22:26:24 -0800

    fix: `request.end(callback)`

--- index.js
+++ index.js
@@ -25,9 +25,14 @@ export default {
       const requestBodyChunks = [];
       for (const method of ["write", "end"]) {
         const originalMethod = interceptedRequest[method];
-        interceptedRequest[method] = function (chunk) {
-          if (chunk) requestBodyChunks.push(Buffer.from(chunk));
-          return originalMethod.call(this, chunk);
+        interceptedRequest[method] = function (chunk, encoding, callback) {
+          // chunk argument may be set to the callback function, see
+          // https://github.com/nodejs/node/blob/9e1a08057a0cd803d0878ed4b87774b5f84d6f0a/lib/_http_outgoing.js#L834-L841
+          if (chunk && typeof chunk !== "function") {
+            requestBodyChunks.push(Buffer.from(chunk));
+          }
+
+          return originalMethod.call(this, chunk, encoding, callback);
         };
       }
 
