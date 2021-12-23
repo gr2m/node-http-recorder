dab0504c0df6d67803c71f14a2f10cbb8ad23971
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-12-22 22:03:25 -0800

    fix(types): improve types and descriptions

--- index.d.ts
+++ index.d.ts
@@ -1,28 +1,62 @@
+import { EventEmitter } from "events";
 import http from "http";
 
-declare const httpRecorder: {
-  start: () => typeof httpRecorder;
-  stop: () => typeof httpRecorder;
-  addListener: (
-    event: "record",
-    listener: RecordHandler
-  ) => typeof httpRecorder;
-  removeListener: (
-    event: "record",
-    listener: RecordHandler
-  ) => typeof httpRecorder;
-  removeAllListeners: () => typeof httpRecorder;
-};
+declare class Recorder extends EventEmitter {
+  /**
+   * Start recording and emitting "record" events.
+   * No-op if already recording.
+   */
+  start: () => this;
 
-export default httpRecorder;
+  /**
+   * Stop recording and emitting "record" events.
+   * No-op if not recording.
+   */
+  stop: () => this;
+
+  /**
+   * Subscribe to the "record" event, emitted each time a response closes.
+   */
+  addListener: (event: "record", listener: RecordHandler) => this;
+
+  /**
+   * Unsubscribe from the "record" event using the same listener function
+   * that was used when subscribing.
+   */
+  removeListener: (event: "record", listener: RecordHandler) => this;
+
+  /**
+   * Remove all listeners from the "record" event.
+   */
+  removeAllListeners(): this;
+}
+
+declare const recorder: Recorder;
+
+export default recorder;
 
 export interface RecordHandler {
   (options: RecordHandlerOptions): void | Promise<void>;
 }
 
 export type RecordHandlerOptions = {
+  /**
+   * The http(s) request that was sent.
+   */
   request: http.ClientRequest;
+
+  /**
+   * The http(s) response that was received
+   */
   response: http.IncomingMessage;
+
+  /**
+   * Array of buffers from the request body.
+   */
   requestBody: Buffer[];
+
+  /**
+   * Array of buffers from the response body.
+   */
   responseBody: Buffer[];
 };
